4c6e3d4b90a4c30a39ac6c4b4b6707eebbeceb8e
TonRichards <58299854+TonRichards@users.noreply.github.com>
2023-04-13 13:26:14 +0700

    feature: designing database

---
+++ database/migrations/2023_04_13_054645_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('name');
+            $table->text('body')->nullable();
+            $table->unsignedBigInteger('space_id');
+            $table->unsignedBigInteger('task_id')->nullable();
+            $table->unsignedBigInteger('status_id');
+            $table->unsignedInteger('type_id');
+            $table->unsignedBigInteger('user_id');
+            $table->dateTime('estimated_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
