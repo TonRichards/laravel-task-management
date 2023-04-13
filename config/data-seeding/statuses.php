ffe1519acfbd73dec70a21a9a83245621ff3cea0
TonRichards <58299854+TonRichards@users.noreply.github.com>
2023-04-13 21:52:23 +0700

    feature: data seeding

---
+++ config/data-seeding/statuses.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'data' => [
+        [
+            'name' => 'todo',
+            'display_name' => 'Todo',
+        ],[
+            'name' => 'in-progress',
+            'display_name' => 'In Progress',
+        ],[
+            'name' => 'done',
+            'display_name' => 'Done',
+        ]
+    ]
+];
---
+++ config/data-seeding/types.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'data' => [
+        [
+            'name' => 'main',
+            'display_name' => 'Main Space',
+            'scope' => 'space',
+        ],[
+            'name' => 'sub',
+            'display_name' => 'Sub Space',
+            'scope' => 'space',
+        ],[
+            'name' => 'main',
+            'display_name' => 'Main Task',
+            'scope' => 'task',
+        ],[
+            'name' => 'sub',
+            'display_name' => 'Sub Task',
+            'scope' => 'task',
+        ]
+    ]
+];
